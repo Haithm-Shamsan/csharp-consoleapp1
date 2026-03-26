c831b41d7562b73287b1f4bae0c96adf5fde6440
Haithm-Shamsan <Haithm-Shamsan@users.noreply.github.com>
2026-03-26 18:41:45 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Cache;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+
+        
+        static void Main(string[] args)
+        {   
+           
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
